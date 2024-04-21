201a66e42dfe59d058e06e2e47ba18337270bead
Vjictor <vjtelles@uncg.edu>
2024-04-21 16:14:12 -0400

    Merge branch 'main' of https://github.com/glemerich/CIC

--- CIC_Application/src/main/java/com/csc340/CIC/admin/UserReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csc340.CIC.admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserReportRepository extends JpaRepository<UserReport, Long> {
-    // Additional query methods can be defined here if needed
-}
