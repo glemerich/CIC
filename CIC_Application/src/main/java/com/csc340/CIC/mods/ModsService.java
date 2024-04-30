893196c1b4f4e1d5a5a538a6c2d518f46f0272fc
glemerich <glemerich@uncg.edu>
2024-04-30 01:17:09 -0400

    Merge changes

--- CIC_Application/src/main/java/com/csc340/CIC/mods/ModsService.java
+++ CIC_Application/src/main/java/com/csc340/CIC/mods/ModsService.java
@@ -3,27 +3,29 @@ package com.csc340.CIC.mods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.csc340.CIC.comment.Comment;
-import com.csc340.CIC.comment.CommentService;
+import com.csc340.CIC.comment.CommentRepository;
+
 import java.util.List;
 
 @Service
 public class ModsService {
     
-    private final ModsRepository modsRepository;
-
     @Autowired
-    public ModsService(ModsRepository modsRepository) {
-        this.modsRepository = modsRepository;
+    private final CommentRepository commentRepository;
+
+    
+    public ModsService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
     }
 
     // Method to get reported comments
     public List<Comment> getReportedComments() {
-        return modsRepository.findByCommentReportTrue();
+        return commentRepository.findByReportedStatusTrue();
     }
 
     // Method to delete reported comment by ID
     public void deleteByID(long commentID) {
-        modsRepository.deleteById(commentID);
+        commentRepository.deleteById(commentID);
     }
 
     
